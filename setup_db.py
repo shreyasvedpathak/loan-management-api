e0923115405170a14f6bcc588acd9d85be4fb033
Shreyas Vedpathak <shreyasvedpathak@gmail.com>
2021-07-08 20:47:06 +0530

    Added gitignore and few other changes

--- setup_db.py
+++ setup_db.py
@@ -1,5 +1,6 @@
 from app import db, create_app
 from app.config import Config
+from app.constants import ROLE
 from app.customer.models import Users
 
 
@@ -13,18 +14,19 @@ def setup_db():
         ADMIN = Users(username=Config.ADMIN_USERNAME,
                       email=Config.ADMIN_EMAIL,
                       contact=Config.ADMIN_CONTACT,
-                      password=Config.ADMIN_PASSWORD)
+                      password=Config.ADMIN_PASSWORD,
+                      role=ROLE['ADMIN'])
 
         db.session.add(ADMIN)
         db.session.commit()
 
         print("Database created.")
         print(f"""
-              Added ADMIN:
-              ADMIN <{dict(username=Config.ADMIN_USERNAME,
+ADMIN <{dict(username=Config.ADMIN_USERNAME,
                       email=Config.ADMIN_EMAIL,
                       contact=Config.ADMIN_CONTACT,
-                      password=Config.ADMIN_PASSWORD)}>""")
+                      password=Config.ADMIN_PASSWORD,
+                      role=ROLE['ADMIN'])}>""")
 
 
 if __name__ == "__main__":
